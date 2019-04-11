c0309922b92b8220c672e63f0d94840cdbae106a
Ananya Sridhar <ananyasridhar@MacBook-Pro-7.lw.hs>
2019-04-11 13:30:57 -0700

    Update sketch.js

--- 5-animated-sprites/sketch.js
+++ 5-animated-sprites/sketch.js
@@ -20,6 +20,7 @@ function setup() {
 
 function draw() {
   background(200, 255, 255);
+  background(255, 255, 255);
 
   //specify the animation instance and its x,y position
   //animation() will update the animation frame as well
